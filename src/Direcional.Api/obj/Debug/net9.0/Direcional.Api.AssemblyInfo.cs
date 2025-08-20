afb6732705f5318be7c85e796ca7afb0406dbe39
Italo Brunno <italo.leite@mgs.srv.br>
2025-08-20 14:44:20 -0300

    feat: finsh configurations

--- src/Direcional.Api/obj/Debug/net9.0/Direcional.Api.AssemblyInfo.cs
+++ src/Direcional.Api/obj/Debug/net9.0/Direcional.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Direcional.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e8dedab5c0057e9abd4f90a768acf3505a4eaf5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef790e71576570c2e44df2de03d088bf42bcb7b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Direcional.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Direcional.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
