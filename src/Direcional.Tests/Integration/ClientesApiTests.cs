165095b5f7e42350159fcbd60cded4f7136a5e2c
Italobgr <italobrunobgr@gmail.com>
2025-08-21 23:08:29 -0300

    test: init ClientTest

---
+++ src/Direcional.Tests/Integration/ClientesApiTests.cs
@@ -0,0 +1,9 @@
+using System.Net;
+using System.Net.Http.Json;
+using FluentAssertions;
+using Xunit;
+
+namespace Direcional.Tests.Integration;
+
+public class ClientesApiTests : IClassFixture<CustomWebAppFactory>
+{ }
